c5dc469cf765379edb00631cbc9aacecf1c54a3b
BA <engineervinceblair@gmail.com>
2020-04-11 18:41:58 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GithubComponent } from './github/github.component';
+import { HomeComponent } from './home/home.component';
+import { LandingComponent } from './landing/landing.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HightlightDirective } from './hightlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GithubComponent,
+    HomeComponent,
+    LandingComponent,
+    NavbarComponent,
+    HightlightDirective
   ],
   imports: [
     BrowserModule,
